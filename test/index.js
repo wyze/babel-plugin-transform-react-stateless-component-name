33db84e1591690f1103da1fd7f8d72e99dc6e045
Neil Kistner <neil.kistner@gmail.com>
2016-12-10 12:54:05 -0600

    Move tests to be in same order of fixtures

--- test/index.js
+++ test/index.js
@@ -17,55 +17,6 @@ const snapshotMacro = ( t, fixtureDir, fixtureFile ) => {
 }
 
 test(
-  'ignores default function block when not returning JSX',
-  snapshotMacro,
-  'export-default-with-function-block-no-jsx',
-)
-
-test(
-  'ignores default function block when no return statement',
-  snapshotMacro,
-  'export-default-function-block-without-return',
-)
-
-test(
-  'default export is a function block returning JSX',
-  snapshotMacro,
-  'export-default-with-function-block',
-)
-
-test(
-  'ignores default function when not returning JSX',
-  snapshotMacro,
-  'export-default-no-jsx',
-)
-
-test(
-  'ignores default function block when not returning JSX',
-  snapshotMacro,
-  'export-default-no-jsx-block',
-)
-
-test(
-  'sets function name for default export based on filename',
-  snapshotMacro,
-  'export-default',
-)
-
-test(
-  'sets displayName for assignment default export',
-  snapshotMacro,
-  'export-default-assignment',
-)
-
-test(
-  'sets default export from folder name when index.js',
-  snapshotMacro,
-  'export-default-from-indexjs',
-  'index.js',
-)
-
-test(
   'sets displayName for assigned functional component',
   snapshotMacro,
   'assigned-component',
@@ -78,33 +29,15 @@ test(
 )
 
 test(
-  'sets displayName for named export functional component',
+  'sets function name for default export based on filename',
   snapshotMacro,
-  'named-export',
+  'export-default',
 )
 
 test(
-  'sets displayName for named export functional component with block',
+  'sets displayName for assignment default export',
   snapshotMacro,
-  'named-export-block',
-)
-
-test(
-  'ignores when jsx is assigned to a variable and not a component/function',
-  snapshotMacro,
-  'ignore-assigned-jsx',
-)
-
-test(
-  'ignores when displayName is already set on function',
-  snapshotMacro,
-  'ignore-existing-displayname',
-)
-
-test(
-  'ignores when displayName is already set on default export function',
-  snapshotMacro,
-  'export-default-ignore-existing-displayname',
+  'export-default-assignment',
 )
 
 test(
@@ -123,4 +56,71 @@ test(
   'sets displayName when if/else block returns JSX',
   snapshotMacro,
   'export-default-dynamic-return-block',
+)
+
+test(
+  'sets default export from folder name when index.js',
+  snapshotMacro,
+  'export-default-from-indexjs',
+  'index.js',
+)
+
+test(
+  'ignores default function block when no return statement',
+  snapshotMacro,
+  'export-default-function-block-without-return',
+)
+
+test(
+  'ignores when displayName is already set on default export function',
+  snapshotMacro,
+  'export-default-ignore-existing-displayname',
+)
+
+test(
+  'ignores default function when not returning JSX',
+  snapshotMacro,
+  'export-default-no-jsx',
+)
+
+test(
+  'ignores default function block when not returning JSX',
+  snapshotMacro,
+  'export-default-no-jsx-block',
+)
+
+test(
+  'default export is a function block returning JSX',
+  snapshotMacro,
+  'export-default-with-function-block',
+)
+
+test(
+  'ignores default function block when not returning JSX',
+  snapshotMacro,
+  'export-default-with-function-block-no-jsx',
+)
+
+test(
+  'ignores when jsx is assigned to a variable and not a component/function',
+  snapshotMacro,
+  'ignore-assigned-jsx',
+)
+
+test(
+  'ignores when displayName is already set on function',
+  snapshotMacro,
+  'ignore-existing-displayname',
+)
+
+test(
+  'sets displayName for named export functional component',
+  snapshotMacro,
+  'named-export',
+)
+
+test(
+  'sets displayName for named export functional component with block',
+  snapshotMacro,
+  'named-export-block',
 )
