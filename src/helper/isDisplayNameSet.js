a853f804b1ddb0633a39559627baf8538a0ab348
Thomas Grainger <tagrain@gmail.com>
2017-11-07 16:51:37 +0000

    Remove all istanbul ignores (#10)

--- src/helper/isDisplayNameSet.js
+++ src/helper/isDisplayNameSet.js
@@ -6,21 +6,22 @@ const isDisplayNameSet = (
   statement: NodePath,
   displayName: string,
 ): boolean => {
+  const displayNameSetInExpr = (sibling: NodePath): boolean => {
+    const expression: NodePath = sibling.get('expression')
+    const member: NodePath = sibling.get('expression.left')
+
+    return !!(
+      expression.isAssignmentExpression() &&
+      member.get('object').isIdentifier({ name: displayName }) &&
+      member.get('property').isIdentifier({ name: 'displayName' })
+    )
+  }
+
   for ( let i = statement.container.length; i > -1; i -= 1 ) {
     const sibling: NodePath = statement.getSibling(i)
 
-    if ( sibling.isExpressionStatement() ) {
-      const expression: NodePath = sibling.get('expression')
-      const member: NodePath = sibling.get('expression.left')
-
-      /* istanbul ignore else */
-      if (
-        expression.isAssignmentExpression() &&
-        member.get('object').isIdentifier({ name: displayName }) &&
-        member.get('property').isIdentifier({ name: 'displayName' })
-      ) {
-        return true
-      }
+    if ( sibling.isExpressionStatement() && displayNameSetInExpr(sibling) ) {
+      return true
     }
   }
 
